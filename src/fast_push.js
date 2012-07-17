59b9802ddbe5f7c45dfa557539853fa7672604a3
Tom MacWright <tom@macwright.org>
2012-07-17 19:04:44 -0400

    Fixup fast_push

--- src/fast_push.js
+++ src/fast_push.js
@@ -2,7 +2,7 @@ if (typeof clustr === 'undefined') clustr = {};
 // Original implementation by Dave Cole @dhcole
 // Formulation by Gregor Aisch
 // http://vis4.net/blog/posts/clean-your-symbol-maps/
-clustr.fast_merge = function(x, getRadius, addPoints, zoom) {
+clustr.fast_push = function(x, getRadius, zoom) {
     var features = x.slice();
     var res = clustr.resolutions[zoom];
     getRadius = clustr.functor(getRadius);
@@ -16,35 +16,36 @@ clustr.fast_merge = function(x, getRadius, addPoints, zoom) {
     var q = quadtree(features);
 
     function collide(node) {
-      var r = (getRadius(node) * res) + 10,
+      function gr(n) { return res * getRadius(n); };
+      node.radius = gr(node);
+      var r = node.radius + 10,
           nx1 = node.x - r,
           nx2 = node.x + r,
           ny1 = node.y - r,
           ny2 = node.y + r;
       return function(quad, x1, y1, x2, y2) {
         if (quad.point && (quad.point !== node)) {
+          quad.point.radius = gr(quad.point);
           var x = node.x - quad.point.x,
               y = node.y - quad.point.y,
               l = Math.sqrt(x * x + y * y),
-              r = 10 + ((getRadius(node) + getRadius(quad.point)) * res);
+              r = 30000 + (node.radius + quad.point.radius);
           if (l < r) {
-            l = (l - r) / l * .5;
-            node.x -= x *= l;
-            node.y -= y *= l;
-            quad.point.x += x;
-            quad.point.y += y;
-          }
+              l = (l - r) / l * .5;
+              if (node.radius > quad.point.radius) {
+                quad.point.x += x * l;
+                quad.point.y += y * l;
+              } else {
+                node.x -= x * l;
+                node.y -= y * l;
+              }
+            }
         }
         return x1 > nx2
             || x2 < nx1
             || y1 > ny2
             || y2 < ny1;
       };
-    }
-
-    var n = features.length, i = 0;
-    while (++i < n) {
-        q.visit(collide(features[i]));
     }
 
     var n = features.length, i = 0;
